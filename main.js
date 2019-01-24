8ebb7c3007ef436af8f1dab67edac4ae8512a3b9
mjsumpter <mjsumpter@gmail.com>
2019-01-24 18:56:25 -0500

    added radio buttons for installments to index.html

--- main.js
+++ main.js
@@ -1,162 +1,162 @@
-
-const reRender = require('./src/sqlToHtml');
-
+const reRender = require("./src/sqlToHtml");
 
 const electron = require("electron");
-const url = require('url');
-const path = require('path');
+const url = require("url");
+const path = require("path");
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 //SET ENV -> production
-// process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = "production";
 
 let mainWindow;
 
 // listen for app to be ready
-app.on('ready', function() {
-    //generate HTML
-    //create new window
-    mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 1000
-    });
-    // Load html into window
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    // Quit app when closed
-    mainWindow.on('closed', function(){
-        app.quit();
-    });
-    // Build menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    // Insert menu
-    Menu.setApplicationMenu(mainMenu);
+app.on("ready", function() {
+  //generate HTML
+  //create new window
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 1000
+  });
+  // Load html into window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // Quit app when closed
+  mainWindow.on("closed", function() {
+    app.quit();
+  });
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
 
-function createQuoteWindow(email){
-    //create new window
-    quoteWindow = new BrowserWindow({
-        width: 800,
-        height: 700,
-        title: 'Order Quote'
-    });
-    // Load html into window
-    quoteWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'quote.html'),
-        protocol:'file:',
-        slashes: true
-    }));
+function createQuoteWindow(email) {
+  //create new window
+  quoteWindow = new BrowserWindow({
+    width: 800,
+    height: 700,
+    title: "Order Quote"
+  });
+  // Load html into window
+  quoteWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "quote.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
 
-    // Build menu from template
-    const quoteMenu = Menu.buildFromTemplate(quoteMenuTemplate);
-    // Insert menu
-    quoteWindow.setMenu(quoteMenu);
+  // Build menu from template
+  const quoteMenu = Menu.buildFromTemplate(quoteMenuTemplate);
+  // Insert menu
+  quoteWindow.setMenu(quoteMenu);
 
-    // add email html from ipcRenderer
-    quoteWindow.webContents.on('did-finish-load', () => {
-        quoteWindow.webContents.send('email', email);
-    });
+  // add email html from ipcRenderer
+  quoteWindow.webContents.on("did-finish-load", () => {
+    quoteWindow.webContents.send("email", email);
+  });
 
-    //garbage collection handle
-    quoteWindow.on('close', () => {
-        quoteWindow = null;
-    });
+  //garbage collection handle
+  quoteWindow.on("close", () => {
+    quoteWindow = null;
+  });
 }
 
 // Catch email
-ipcMain.on('email', function(e, email){
-    createQuoteWindow(email);   
+ipcMain.on("email", function(e, email) {
+  createQuoteWindow(email);
 });
 
 // Create main menu template
 const mainMenuTemplate = [
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Clear Page',
-                accelerator: process.platform == 'darwin' ? 'Command+Z' : 'Ctrl+Z',
-                click(){
-                    mainWindow.reload();
-                }
-            },
-            {
-                label: 'Reload Database',
-                accelerator: process.platform == 'darwin' ? 'Command+R' : 'Ctrl+R',
-                click(){
-                    reRender.loadDB('./db/products.db', () => {
-                        mainWindow.reload();
-                    });
-                }
-            },
-            {
-                label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click(){
-                    app.quit();
-                }
-            }
-        ]
-    }
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Clear Page",
+        accelerator: process.platform == "darwin" ? "Command+Z" : "Ctrl+Z",
+        click() {
+          mainWindow.reload();
+        }
+      },
+      {
+        label: "Reload Database",
+        accelerator: process.platform == "darwin" ? "Command+R" : "Ctrl+R",
+        click() {
+          reRender.loadDB("./db/products.db", () => {
+            mainWindow.reload();
+          });
+        }
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
 ];
 
 // Create quote menu template
 const quoteMenuTemplate = [
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click() {
-                    quoteWindow.close();
-                }
-            }
-        ]
-    },
-    {
-        label: 'Edit',
-        submenu: [
-            { 
-                label: "Copy", 
-                accelerator: process.platform == 'darwin' ? 'Command+C' : 'Ctrl+C',
-                selector: "copy:" 
-            },
-            { 
-                label: "Select All", 
-                accelerator: process.platform == 'darwin' ? 'Command+A' : 'Ctrl+A',
-                selector: "selectAll:" 
-            }
-        ]
-    }
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          quoteWindow.close();
+        }
+      }
+    ]
+  },
+  {
+    label: "Edit",
+    submenu: [
+      {
+        label: "Copy",
+        accelerator: process.platform == "darwin" ? "Command+C" : "Ctrl+C",
+        selector: "copy:"
+      },
+      {
+        label: "Select All",
+        accelerator: process.platform == "darwin" ? "Command+A" : "Ctrl+A",
+        selector: "selectAll:"
+      }
+    ]
+  }
 ];
 
 // if mac, add empty object to menu
-if(process.platform == 'darwin')
-{
-    mainMenuTemplate.unshift({});
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
 }
 
 // Add developer tools item if not in production
-if(process.env.NODE_ENV !== 'production')
-{
-    mainMenuTemplate.push({
-        label: 'Developer Tools',
-        submenu: [
-            {
-                label: 'Toggle DevTools',
-                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-                click(item, focusedWindow){
-                    focusedWindow.toggleDevTools();
-                }
-            },
-            {
-                role: 'reload'
-            }
-        ]
-    });
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
 }
--- src/sqlToHtml.js
+++ src/sqlToHtml.js
@@ -1,29 +1,29 @@
-const render = require('./render');
-const sqlite3 = require('sqlite3');
-const SqliteToJson = require('sqlite-to-json');
+const render = require("./render");
+const sqlite3 = require("sqlite3");
+const SqliteToJson = require("sqlite-to-json");
 
 // // for local testing
-// const db = '../db/products.db';
+// const db = "../db/products.db";
 // loadDBLocal(db);
 
 function loadDBLocal(path) {
-    const exporter = new SqliteToJson({
-        client: new sqlite3.Database(path)
-    });
-    exporter.all((err, all) => {
-        render.renderHTMLLocal(all.productList);
-    });    
+  const exporter = new SqliteToJson({
+    client: new sqlite3.Database(path)
+  });
+  exporter.all((err, all) => {
+    render.renderHTMLLocal(all.productList);
+  });
 }
 
 // for electron rendering
 function loadDB(path, callback) {
-    const exporter = new SqliteToJson({
-        client: new sqlite3.Database(path)
-    });
-    exporter.all((err, all) => {
-        render.renderHTML(all.productList);
-    });
-    callback();
+  const exporter = new SqliteToJson({
+    client: new sqlite3.Database(path)
+  });
+  exporter.all((err, all) => {
+    render.renderHTML(all.productList);
+  });
+  callback();
 }
 
 module.exports.loadDB = loadDB;
